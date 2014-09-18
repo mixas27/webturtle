e36f6aa9aa986a26bb8260d4302370afc3760284
Mikhail Stryzhonok <mixas.27@yandex.ru>
2014-09-18 12:04:18 +0300

    Application entry point

---
+++ src/main/java/org/mixas/webturtle/WebTurtle.java
@@ -0,0 +1,13 @@
+package org.mixas.webturtle;
+
+/**
+ * Application entry point
+ *
+ * @author Mikhail Stryzhonok
+ */
+public class WebTurtle {
+
+    public static void main(String[] args) {
+        System.out.println("Application startup");
+    }
+}
